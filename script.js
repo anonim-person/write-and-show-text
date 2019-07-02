b0c0ba95329df8c7a3b6830566bd8a7acae78fbf
anonim-person <50899337+anonim-person@users.noreply.github.com>
2019-07-02 18:54:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ function on(){
     p.appendChild(node);   
     window.scrollTo(0,document.body.scrollHeight);
 }
+function red(){
+    var a = document.getElementById('non');
+    document.body.style.background = "purple";
+    a.style.color = "red";
+}
+function blue(){
+    var a = document.getElementById('non');
+    document.body.style.background = "yellow";
+    a.style.color = "blue";
+}
+function green(){
+    var a = document.getElementById('non');
+    document.body.style.background = "lightblue";
+    a.style.color = "green";
+}
+function maro(){
+    var a = document.getElementById('non');
+    a.style.color = "brown";
+    document.body.style.background = "pink";
+}
+function yellow(){
+    var a = document.getElementById('non');
+    a.style.color = "yellow";
+    document.body.style.background = "green";
+}
+function pink(){
+    var a = document.getElementById('non');
+    a.style.color = "pink";
+    document.body.style.background = "red";
+}
+function black(){
+    var a = document.getElementById('non');
+    a.style.color = "black";
+    document.body.style.background = "grey";
+}
