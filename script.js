4abfb6ec0052a1c4d12339842561a549d803013f
anonim-person <50899337+anonim-person@users.noreply.github.com>
2019-07-02 20:19:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,42 +3,66 @@ function on(){
     var p = document.getElementById("non");
     var node = document.createElement("p");       
     var textnode = document.createTextNode(n);
-    node.appendChild(textnode);                             
-    p.appendChild(node);   
+    if(n == ""){
+        alert("Te rog scrie ceva...");
+    }else{
+        node.appendChild(textnode);                             
+        p.appendChild(node);   
+    }
     window.scrollTo(0,document.body.scrollHeight);
 }
 function red(){
     var a = document.getElementById('non');
-    document.body.style.background = "purple";
     a.style.color = "red";
 }
 function blue(){
     var a = document.getElementById('non');
-    document.body.style.background = "yellow";
     a.style.color = "blue";
 }
 function green(){
     var a = document.getElementById('non');
-    document.body.style.background = "lightblue";
     a.style.color = "green";
 }
 function maro(){
     var a = document.getElementById('non');
     a.style.color = "brown";
-    document.body.style.background = "pink";
 }
 function yellow(){
     var a = document.getElementById('non');
     a.style.color = "yellow";
-    document.body.style.background = "green";
 }
 function pink(){
     var a = document.getElementById('non');
     a.style.color = "pink";
-    document.body.style.background = "red";
 }
 function black(){
     var a = document.getElementById('non');
     a.style.color = "black";
+}
+function purple(){
+    document.body.style.background = "purple";
+}
+function grey(){
     document.body.style.background = "grey";
+}
+function white(){
+    document.body.style.background = "white";
+}
+function orange(){
+    document.body.style.background = "orange";
+}
+function beige(){
+    document.body.style.background = "beige";
+}
+function golden(){
+    document.body.style.background = "goldenrod";
+}
+function amethyst(){
+    document.body.style.background = "rgb(59, 134, 156)";
+}
+function bacs(){
+    var a = Math.floor(Math.random() * 250);
+    var b = Math.floor(Math.random() * 250);
+    var c = Math.floor(Math.random() * 250);
+    document.body.style.background = "rgb(" + a + "," + b + "," + c + ")";
 }
